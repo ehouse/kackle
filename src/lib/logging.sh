f54f74086749b532f89c07ac6dbd8bda1948df1b
Ethan House <ehouse@fastmail.com>
2018-03-04 15:29:17 -0500

    Adding docstrings to logging library

--- src/lib/logging.sh
+++ src/lib/logging.sh
@@ -1,14 +1,30 @@
+############################
+# Event proved fatal, log and exit with error code
+# Arguments:
+#  Exit Message
+#  Exit Code
+############################
 logging::fatal() {
     local -r msg=${1:-"Exiting!"}
     echo -e "ERROR: $msg"
     exit ${2:-1}
 }
 
+############################
+# Warn of an event, non fatal and will continue
+# Arguments:
+#  Warning Message
+############################
 logging::warning() {
     local -r msg=${1:-"Proceding as usua"}
     echo -e "WARNING: $msg"
 }
 
+############################
+# Log an event
+# Arguments:
+#  Message
+############################
 logging::info() {
     local -r msg=${1:-"An event occured"}
     echo -e "INFO: $msg"
